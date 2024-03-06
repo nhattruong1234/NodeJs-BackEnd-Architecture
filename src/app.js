43c3bfbf2b1d07600c256ef15626335c29020c6a
nhattruong1234 <nguyenphamnhattruong2001@gmail.com>
2024-03-06 19:58:10 +0700

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const compression = require("compression");
+const express = require("express");
+const { default: helmet } = require("helmet");
+const morgan = require("morgan");
+const { compile } = require("morgan");
+const app = express();
+
+// init middlewares
+app.use(morgan("dev"));
+// app.use(morgan("common"));
+// app.use(morgan("combined"));
+// app.use(morgan("short"));
+// app.use(morgan("tiny"));
+app.use(helmet());
+app.use(compression());
+
+app.get("/", (req, res, next) => {
+  const strCompress = "Hello Hi NT";
+  return res
+    .status(500)
+    .json({ message: "Hello NT", metadata: strCompress.repeat(1000) });
+});
+module.exports = app;
